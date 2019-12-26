a1c8471112f16ec69163a69509df0f95a3266fb8
SanDiegos <52674560+SanDiegos@users.noreply.github.com>
2019-12-26 15:31:10 +0100

    Add files via upload

---
+++ src/test/java/com/djedra/oAuth2authorizationservice/OAuth2AuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djedra.oAuth2authorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2AuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

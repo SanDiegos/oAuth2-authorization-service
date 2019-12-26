8a1dae8d8b504b1e4984cee1c0f97cce5152119a
die-g <die-g@192.168.0.20>
2019-12-26 22:43:53 +0100

    First version

--- src/main/java/com/djedra/oAuth2authorizationservice/OAuth2AuthorizationServiceApplication.java
+++ src/main/java/com/djedra/oAuth2authorizationservice/OAuth2AuthorizationServiceApplication.java
@@ -2,8 +2,10 @@ package com.djedra.oAuth2authorizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class OAuth2AuthorizationServiceApplication {
 
 	public static void main(String[] args) {
